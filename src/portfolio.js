6cf0424d102620a3594d5e0c2cfb51ae360f846b
Ajay Pownraj <ajaypownraj@gmail.com>
2021-06-30 18:00:03 +0530

    First commit of completed portfolio-v2

---
+++ src/portfolio.js
@@ -0,0 +1,432 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Ajay Pownraj",
+  logo_name: "<-- Aj --/>",
+  nickname: "aj",
+  full_name: "Ajay Pownraj",
+  subTitle:
+    "UI / Front End Developer, I build things for the web 🔥. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1QMFrEnGJ5L45n1eycwhdlRfx6nA5Tmma/view?usp=sharing",
+  mail: "mailto:ajaypownraj@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Ajay-Pownraj",
+  linkedin: "https://www.linkedin.com/in/ajaypownraj/",
+  gmail: "ajaypownraj@gmail.com",
+  facebook: "https://www.facebook.com/ajaypownraj",
+  twitter: "https://twitter.com/Ajay_pownraj",
+  instagram: "https://www.instagram.com/ajaypownraj/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Front End Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        // {
+        //   skillName: "VueJS",
+        //   fontAwesomeClassname: "simple-icons:vue",
+        //   style: {
+        //     color: "#7377AD",
+        //   },
+        // },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        // {
+        //   skillName: "Android",
+        //   fontAwesomeClassname: "simple-icons:android",
+        //   style: {
+        //     color: "#3DDC84",
+        //   },
+        // },
+        // {
+        //   skillName: "Java",
+        //   fontAwesomeClassname: "simple-icons:java",
+        //   style: {
+        //     color: "#f89820",
+        //   },
+        // },
+        // {
+        //   skillName: "Flutter",
+        //   fontAwesomeClassname: "simple-icons:flutter",
+        //   style: {
+        //     color: "#02569B",
+        //   },
+        // },
+        // {
+        //   skillName: "Dart",
+        //   fontAwesomeClassname: "simple-icons:dart",
+        //   style: {
+        //     color: "#29B0EE",
+        //   },
+        // },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        // {
+        //   skillName: "Wordpress",
+        //   fontAwesomeClassname: "simple-icons:wordpress",
+        //   style: {
+        //     color: "#207297",
+        //   },
+        // },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        // {
+        //   skillName: "C",
+        //   fontAwesomeClassname: "simple-icons:c",
+        //   style: {
+        //     color: "#E94E32",
+        //   },
+        // },
+        // {
+        //   skillName: "C++",
+        //   fontAwesomeClassname: "simple-icons:cplusplus",
+        //   style: {
+        //     color: "#E94E32",
+        //   },
+        // },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        // {
+        //   skillName: "AWS",
+        //   fontAwesomeClassname: "simple-icons:amazonaws",
+        //   style: {
+        //     color: "#FF9900",
+        //   },
+        // },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        // {
+        //   skillName: "PostgreSQL",
+        //   fontAwesomeClassname: "simple-icons:postgresql",
+        //   style: {
+        //     color: "#336791",
+        //   },
+        // },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        // {
+        //   skillName: "Docker",
+        //   fontAwesomeClassname: "simple-icons:docker",
+        //   style: {
+        //     color: "#1488C6",
+        //   },
+        // },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Kongu Engineering College",
+      subtitle: "Computer Science and Engineering",
+      logo_path: "kec_logo.png",
+      alt_name: "KEC LOGO",
+      duration: "2015 - 2019",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+        "⚡ Secretary in Computer Socitey of India-2019, Conducted and Organised various work shop and Event. "
+      ],
+      website_link: "https://kongu.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+   
+    {
+      title: "Sample Certificate",
+      subtitle: "Sample Certificate",
+      logo_path: "postman.png",
+      certificate_link:
+        "Sample Certificate",
+      alt_name: "Sample Certificate",
+      // color_code: "#f36c3d",
+      color_code: "#fffbf3",
+    },
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "Innovative Front-End Developer with 2+ years’ of experience building and maintaining responsive website in the industry. ",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "UI / Font-End Developer",
+          company: "Accenture",
+          company_url: "https://www.accenture.com/in-en",
+          logo_path: "accenture-logo.jpg",
+          duration: "May 2019 - Present",
+          location: "Bangalore, India.",
+          description:
+            "Developed and maintained code for client websites primarily, Manually tested sites in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness, Implementing a mobile first approach to existing website, Expert at designing and developing cross browser compatible website, Involved in prototyping and in giving solutions to complex technical problems.",
+          color: "#0071C5",
+        },
+        {
+          title: "Intership",
+          company: "Logic Research Labs",
+          company_url: "https://logicresearchlabs.com/",
+          logo_path: "lrl-logo.png",
+          duration: "Jan 2019 - April 2019",
+          location: "Kumbakonam, Tamil Nadu, India",
+          description:
+            "Degined simple web application for better user experience, Implemented features like Notifications, Admin Dashboard.",
+          color: "#ee3c26",
+        }
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Computer Society of India",
+          company: "CSE - Student Community",
+          company_url: "http://csi-india.org.in/",
+          logo_path: "csi-logo.jpg",
+          duration: "Feb 2016 - 19",
+          location: "Kongu Engineering College",
+          description:
+            "Conducted and Organised various work shop and Event. Spread Awareness of new Technologies and new Opportunities to Students and Grow Skillenza Community. ",
+          color: "#4285F4",
+        },
+        {
+          title: "National Service Scheme (NSS)",
+          company: "Public Service Program",
+          company_url: "https://nss.gov.in/",
+          logo_path: "nss-logo.jpg",
+          duration: "Feb 2016",
+          location: "Kongu Engineering College",
+          description: "Indian government-sponsored flagship for public service program conducted by the Ministry of Youth Affairs and Sports of the Government of India",
+          color: "#196acf",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "ajaypownraj.JPG",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://twitter.com/Harikrushn9",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Sample Project",
+      url: "https://github.com/Ajay-Pownrajt",
+      description: "Sample Description",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
